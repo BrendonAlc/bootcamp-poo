cde7c6ad9ade4448181ed5afbb195206b22d39ee
Brendon Alcantara <brendon_rodrigo@hotmail.com>
2024-06-05 22:10:58 -0300

    Criado projeto com conceito dos pilares de OO, utilizando abstracao, encapsulamento, Heranca e Polimorfismo de Collections, utilizando como base a inscricao de curso em um bootcamp, qual o bootcamp escrito e quais foram concluidos.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,57 @@
+import br.com.brendon.desafio.dominio.*;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso java");
+        curso1.setDescricao("desafios de POO");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso("Curso c#", "Desafios de POO", 5);
+
+        Curso curso3 = new Curso("Curso c", "Desafios de POO", 10);
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(curso3);
+
+        Mentoria mentoria = new Mentoria("Mentoria java", "Mentoria para java", LocalDate.now());
+        System.out.println(mentoria);
+        System.out.println();
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp java developer");
+        bootcamp.setDescricao("Desafio POO com bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devBrendon = new Dev();
+        devBrendon.setNome("Brendon");
+        devBrendon.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos que o dev " + devBrendon.getNome() + " esta inscrito " + devBrendon.getConteudosInscritos());
+        devBrendon.progredir();
+        devBrendon.progredir();
+        System.out.println("--");
+        System.out.println("Conteudos que o dev " + devBrendon.getNome() + " esta inscrito " + devBrendon.getConteudosInscritos());
+        System.out.println("Conteudos que o dev " + devBrendon.getNome() + " esta concluido " + devBrendon.getConteudosConcluidos());
+        System.out.println("XP: " + devBrendon.calcularTotalXp());
+
+        System.out.println("------------------");
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("Joao");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos que dev " + devJoao.getNome() + " está inscrito " + devJoao.getConteudosInscritos());
+        devJoao.progredir();
+        System.out.println("--");
+        System.out.println("Conteudos que dev " + devJoao.getNome() + " está inscrito " + devJoao.getConteudosInscritos());
+        System.out.println("Conteudos que dev " + devJoao.getNome() + " está concluido " + devJoao.getConteudosConcluidos());
+        System.out.println("XP: " + devJoao.calcularTotalXp());
+
+
     }
 }
---
+++ src/br/com/brendon/desafio/dominio/Curso.java
@@ -0,0 +1,36 @@
+package br.com.brendon.desafio.dominio;
+
+public class Curso extends Conteudo {
+    private int cargaHoraria;
+
+    public Curso() {
+    }
+
+    public Curso(String titulo, String descricao, int cargaHoraria) {
+        this.setTitulo(titulo);
+        this.setDescricao(descricao);
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public double calcularXP() {
+        return 0;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
+}
